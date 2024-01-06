ea8ff71e3c4364304a140387eb739ffd3c5b2260
sagar <sagarrajak858@gmail.com>
2024-01-07 02:44:50 +0530

    circuit breaking implemented

--- inventory/src/main/java/com/fancystore/inventory/controller/InventoryController.java
+++ inventory/src/main/java/com/fancystore/inventory/controller/InventoryController.java
@@ -3,6 +3,8 @@ package com.fancystore.inventory.controller;
 
 import com.fancystore.inventory.exception.CustomIllegalArgumentException;
 import com.fancystore.inventory.service.InventoryService;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +15,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/inventory")
+@Slf4j
 public class InventoryController {
     @Autowired
     private InventoryService inventoryService;
@@ -23,11 +26,16 @@ public class InventoryController {
         return this.inventoryService.isInStock(skuCode);
     }
 
+
+    //    @SneakyThrows
     @GetMapping("/items")
     @ResponseStatus(HttpStatus.OK)
     public HashMap<String, Boolean> isItemsIsInStockStock(
         @RequestParam("sku-code") Optional<List<String>> skuCode
     ) throws CustomIllegalArgumentException {
+//        log.info("Wait time started!");
+//        Thread.sleep(1000);
+//        log.info("Wait time finished!");
         return skuCode
                 .map(e -> this.inventoryService.isInStock(e))
                 .orElseThrow(() -> new CustomIllegalArgumentException("sku-code not provided!"));
--- order-service/src/main/java/com/fancystore/orderservice/controller/OrderController.java
+++ order-service/src/main/java/com/fancystore/orderservice/controller/OrderController.java
@@ -6,10 +6,13 @@ import com.fancystore.orderservice.entity.Order;
 import com.fancystore.orderservice.error.CustomIllegalArgumentException;
 import com.fancystore.orderservice.service.OrderService;
 import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.timelimiter.annotation.TimeLimiter;
 import org.aspectj.weaver.ast.Or;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping("/api/order")
@@ -19,13 +22,14 @@ public class OrderController {
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     @CircuitBreaker(name = "inventory", fallbackMethod = "fallbackMethod")
-    public OrderResponse<Order> placeOrder(@RequestBody OrderRequest orderRequest) throws CustomIllegalArgumentException {
+    @TimeLimiter(name = "inventory")
+    public CompletableFuture<OrderResponse<Order>> placeOrder(@RequestBody OrderRequest orderRequest) throws CustomIllegalArgumentException {
         Order order = this.orderService.placeOrder(orderRequest);
-        return new OrderResponse<>(order, "Order successfully!");
+        return CompletableFuture.supplyAsync(() -> new OrderResponse<>(order, "Order successfully!"));
     }
 
-    public OrderResponse<Order> fallbackMethod(@RequestBody OrderRequest orderRequest) throws CustomIllegalArgumentException {
-        return new OrderResponse<>(null, "Something went wrong, unable to place order!");
+    public CompletableFuture<OrderResponse<Order>> fallbackMethod(@RequestBody OrderRequest orderRequest, RuntimeException exception) throws CustomIllegalArgumentException {
+        return CompletableFuture.supplyAsync(() -> new OrderResponse<>(null, "Something went wrong, unable to place order!"));
     }
 
 }
